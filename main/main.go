a28aa96177a3e6e5cc7f1d07290bbf8f263a36f8
mikevin <kevin.michot@ynov.fr>
2024-10-14 15:25:16 +0200

    Fusion de Finalword dans main

--- main/main.go
+++ main/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"Hangman"
+	"fmt"
 )
 
+// Fonction pour lancer la partie de hangman
 func main() {
-	//Print le mot choisis en cryptée
- Hangman.Finalworld()
-}
+    fileName := Hangman.ChooseDifficulty()
 
+    wordList, err := Hangman.GetRandomWord(fileName)
+    if err != nil {
+        fmt.Println("Erreur lors du chargement des mots :", err)
+        return
+    }
+
+    chosenWord := Hangman.ChooseRandomWord(wordList)
+
+    blanks := Hangman.Blankword(chosenWord)
+    fmt.Println("Mot à deviner : ", blanks)
+
+    Hangman.PlayGame(chosenWord, blanks)
+}
