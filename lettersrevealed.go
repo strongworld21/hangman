6d05dbfab693ba6bf3815fb05685d0d88e4bcfac
svictor <victor.serazin@ynov.com>
2024-10-15 17:50:27 +0200

    fonction pour révéler les lettres

---
+++ lettersrevealed.go
@@ -0,0 +1,31 @@
+package Hangman
+
+import (
+    "math/rand"
+    "strings"
+)
+
+func LettersRevealed(chosenWord string) string {
+    length := len(chosenWord)
+    lettersToReveal := (length / 2) - 1 
+    if lettersToReveal <= 0 { //si mot de 2 lettres
+        lettersToReveal = 1
+    }
+
+    revealedIndices := make(map[int]bool)
+
+    for len(revealedIndices) < lettersToReveal {
+        randomIndex := rand.Intn(length)
+        revealedIndices[randomIndex] = true
+    }
+    blanks := []string{}
+    for i, ch := range chosenWord {
+        if revealedIndices[i] {
+            blanks = append(blanks, string(ch))
+        } else {
+            blanks = append(blanks, "_")
+        }
+    }
+    
+    return strings.Join(blanks, " ")
+}
